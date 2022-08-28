29f7ba78880bf05d4705f32e193c92249e6781e3
Anargya Widyadhana <37019778+zephyrzth@users.noreply.github.com>
2022-08-28 09:46:21 +0700

    fix cors

--- route.go
+++ route.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	handlerHttp "github.com/zephyrzth/wdiet-be/handler/http"
 	"github.com/zephyrzth/wdiet-be/usecase"
@@ -10,12 +12,23 @@ func newRouter(usecase usecase.UsecaseInterface) *mux.Router {
 	handlerHttp := handlerHttp.New(usecase)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/restaurants", handlerHttp.GetAllRestaurant).Methods("GET")
-	r.HandleFunc("/restaurants/{id:[a-zA-Z0-9]+}", handlerHttp.GetRestaurantByID).Methods("GET")
-	r.HandleFunc("/register", handlerHttp.Register).Methods("POST")
-	r.HandleFunc("/login", handlerHttp.Login).Methods("POST")
-	r.HandleFunc("/profile/{id:[a-zA-Z0-9]+}", handlerHttp.GetProfile).Methods("GET")
-	r.HandleFunc("/usermenu", handlerHttp.InsertUserMenu).Methods("POST")
+	r.HandleFunc("/restaurants", wrapHandler(handlerHttp.GetAllRestaurant)).Methods("GET")
+	r.HandleFunc("/restaurants/{id:[a-zA-Z0-9]+}", wrapHandler(handlerHttp.GetRestaurantByID)).Methods("GET")
+	r.HandleFunc("/register", wrapHandler(handlerHttp.Register)).Methods("POST")
+	r.HandleFunc("/login", wrapHandler(handlerHttp.Login)).Methods("POST")
+	r.HandleFunc("/profile/{id:[a-zA-Z0-9]+}", wrapHandler(handlerHttp.GetProfile)).Methods("GET")
+	r.HandleFunc("/usermenu", wrapHandler(handlerHttp.InsertUserMenu)).Methods("POST")
 
 	return r
+}
+
+func wrapHandler(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+		handler(w, r)
+	}
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
